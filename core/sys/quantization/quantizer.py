dfbc1d0ebbdd6fd8de79497a20c7ecce74da1afe
dashygo097 <dashygo097@gmail.com>
2025-05-18 16:34:17 +0800

    optimized and add something new to the tracer

---
+++ core/sys/quantization/quantizer.py
@@ -0,0 +1,47 @@
+import os
+from typing import Optional, overload
+
+from torch import nn
+from torch.fx.node import Node
+
+from ..tracer import Tracer
+
+
+class Quantizer(Tracer):
+    def __init__(self, model: nn.Module) -> None:
+        super().__init__(model)
+
+    def parse(self, folder: str = "output/traced", module_name: Optional[str] = None):
+        os.makedirs(folder, exist_ok=True)
+        if module_name is None:
+            self.graph.to_folder(folder, "Quantized" + self.model.__class__.__name__)
+
+        else:
+            self.graph.to_folder(folder, module_name)
+
+    @overload
+    def quantize(self, target: Optional[Node], dtype: str = "int8"):
+        return self.quantize(target, dtype)
+
+    @overload
+    def quantize(self, target: Optional[str], dtype: str = "int8"):
+        return self.quantize(target, dtype)
+
+    @overload
+    def quantize(self, target: type, dtype: str = "int8"):
+        return self.quantize(target, dtype)
+
+    def quantize(
+        self, target: Optional[Node] | Optional[str] | type, dtype: str = "int8"
+    ):
+        if target is None:
+            raise ValueError("Target cannot be None")
+
+        elif isinstance(target, Node):
+            ...
+
+        elif isinstance(target, str):
+            ...
+
+        elif isinstance(target, type):
+            ...
