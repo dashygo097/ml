067081e9e0fd78308fe125993a694b2b3b6eb9bd
dashygo097 <dashygo097@github.com>
2025-05-24 22:02:21 +0800

    add:tripletdataset

--- core/models/cnn/contrast_trainer.py
+++ core/models/cnn/contrast_trainer.py
@@ -1,10 +1,50 @@
+import random
+from collections import defaultdict
 from typing import Dict, List, Tuple
 
 import torch
 from termcolor import colored
 from torch import nn
+from torch.utils.data import Dataset
 
 from .trainer import CNNTrainArgs, CNNTrainer
+
+
+class TripletDataset(Dataset):
+    def __init__(self, ds, transforms=None) -> None:
+        super().__init__()
+        self.ds = ds
+        self.transforms = transforms
+
+        self.class_to_indices = defaultdict(list)
+        for idx, (_, label) in enumerate(self.ds):
+            self.class_to_indices[label].append(idx)
+
+        self.classes = list(self.class_to_indices.keys())
+
+    def __len__(self) -> int:
+        return len(self.ds)
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        anchor, label = self.ds[idx]
+
+        positive_idx = idx
+        while positive_idx == idx:
+            positive_idx = random.choice(self.class_to_indices[label])
+        positive = self.ds[positive_idx]
+
+        negetive_label = random.choice(
+            [_label for _label in self.classes if _label != label]
+        )
+        negative_idx = random.choice(self.class_to_indices[negetive_label])
+        negative = self.ds[negative_idx]
+
+        if self.transforms is not None:
+            anchor = self.transforms(anchor)
+            positive = self.transforms(positive)
+            negative = self.transforms(negative)
+
+        return anchor, positive, negative
 
 
 class SimCLRTrainArgs(CNNTrainArgs):
@@ -23,8 +63,9 @@ class SimCLRTrainer(CNNTrainer):
         args: SimCLRTrainArgs,
         optimizer=None,
         scheduler=None,
+        valid_ds=None,
     ):
-        super().__init__(model, ds, criterion, args, optimizer, scheduler)
+        super().__init__(model, ds, criterion, args, optimizer, scheduler, valid_ds)
 
     def step(self, batch: Tuple[torch.Tensor, ...] | List[torch.Tensor]) -> Dict:
         anchored, positive, negative = batch
