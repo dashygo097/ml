7b720364ba6953358328fc11c7fb2fe7964aceb6
dashygo097 <dashygo097@gmail.com>
2025-06-11 18:01:02 +0800

    add: GQA

--- core/models/transformer/attns/__init__.py
+++ core/models/transformer/attns/__init__.py
@@ -1,5 +1,6 @@
 from .cross_attn import *
 from .functional import *
+from .gqa import *
 from .mha import *
 from .mha2d import *
 from .mla import *
--- hust_ic_sync.sh
+++ hust_ic_sync.sh
@@ -1,8 +1,4 @@
 rsync -avz --exclude='__pycache__' --exclude='.git' --exclude='*.pyc' \
   ./core ./benchmark ./README.md ./requirements.txt lcxie@222.20.98.159:~/dev/ml
-rsync -avz --exclude='__pycache__' --exclude='.git' --exclude='*.pyc' \
-  ./demo/ResNet-Pretrain-Faces lcxie@222.20.98.159:~/dev/ml/demo
-rsync -avz --exclude='__pycache__' --exclude='.git' --exclude='*.pyc' \
-  ./demo/Wav2Lip lcxie@222.20.98.159:~/dev/ml/demo
 rsync -avz --exclude='__pycache__' --exclude='.git' --exclude='*.pyc' --exclude='attempts' --exclude="valid_*"\
-  ./demo/DCGAN-GenMNIST lcxie@222.20.98.159:~/dev/ml/demo
+  ./demo/Hust-IC-LipSync lcxie@222.20.98.159:~/dev/ml/demo
