caaecd4038c56646e69acdd8ce0966ad9bfeff9b
dashygo097 <dashygo097@github.com>
2025-06-12 20:53:41 +0800

    rm:hust_ic_sync.sh

--- hust_ic_sync.sh
+++
@@ -1,6 +0,0 @@
-rsync -avz --exclude='__pycache__' --exclude='.git' --exclude='*.pyc' \
-  ./core ./benchmark ./README.md ./requirements.txt lcxie@222.20.98.159:~/dev/ml
-rsync -avz --exclude='__pycache__' --exclude='.git' --exclude='*.pyc' --exclude='attempts' --exclude="valid_*"\
-  ./demo/Hust-IC-LipSync lcxie@222.20.98.159:~/dev/ml/demo
-rsync -avz --exclude='__pycache__' --exclude='.git' --exclude='*.pyc' --exclude='attempts' --exclude="valid_*"\
-  ./demo/DCGAN-GenMNIST lcxie@222.20.98.159:~/dev/ml/demo
