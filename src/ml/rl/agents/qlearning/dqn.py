fb7a93d7c8294487558a64f4b92819fd8600a246
dashygo097 <dashygo097@gmail.com>
2025-08-19 16:44:56 +0800

    update: replaybuffer

--- src/ml/rl/agents/qlearning/dqn.py
+++ src/ml/rl/agents/qlearning/dqn.py
@@ -1,3 +1,4 @@
+import random
 from typing import Any, Dict
 
 import torch
@@ -5,6 +6,26 @@ from torch import nn
 
 from ...envs import BaseDiscreteEnv
 from ..base import RLAgent
+
+
+class ReplayBuffer:
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.buffer = []
+        self.position = 0
+
+    def push(self, transition: Dict[str, Any]) -> None:
+        if len(self.buffer) < self.capacity:
+            self.buffer.append(transition)
+        else:
+            self.buffer[self.position] = transition
+        self.position = (self.position + 1) % self.capacity
+
+    def sample(self, batch_size: int):
+        return random.sample(self.buffer, batch_size)
+
+    def __len__(self) -> int:
+        return len(self.buffer)
 
 
 class DeepQLearning(RLAgent):
