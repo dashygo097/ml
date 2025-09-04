de6664649832748e8c813405f1ec86468c738cbb
dashygo097 <dashygo097@gmail.com>
2025-09-04 15:52:31 +0800

    checkpoint

---
+++ src/ml/sys/quantization/awq.py
@@ -0,0 +1,13 @@
+from typing import Optional, overload
+
+import torch
+from torch import nn
+
+from .quantizer import Quantizer
+
+
+class AWQ(Quantizer):
+    def __init__(self, model: nn.Module) -> None:
+        super().__init__(model)
+
+    def quantize(self, target: Optional[str] | type): ...
