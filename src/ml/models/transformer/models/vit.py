186051117d4aa55044516aabec6b4f8933835113
dashygo097 <dashygo097@gmail.com>
2025-09-20 15:42:49 +0800

    vit polish

--- src/ml/models/transformer/models/vit.py
+++ src/ml/models/transformer/models/vit.py
@@ -103,85 +103,6 @@ class ViTBackbone(nn.Module):
         x = self.encoder(x)
         return self.post_layernorm(x)
 
-    def from_hf(self, state_dict: Dict[str, torch.Tensor]) -> None:
-        # Load Embedding
-        self.embedding.proj.weight.data = state_dict.pop(
-            "embeddings.patch_embeddings.projection.weight"
-        )
-        self.embedding.proj.bias.data = state_dict.pop(
-            "embeddings.patch_embeddings.projection.bias"
-        )
-        self.embedding.cls_token.data = state_dict.pop("embeddings.cls_token")
-        self.embedding.pos_embedding.data = state_dict.pop(
-            "embeddings.position_embeddings"
-        )
-        # Load Attention
-        for i in range(self.n_layers):
-            query_weight = state_dict.pop(
-                f"encoder.layer.{i}.attention.attention.query.weight"
-            )
-            query_bias = state_dict.pop(
-                f"encoder.layer.{i}.attention.attention.query.bias"
-            )
-            key_weight = state_dict.pop(
-                f"encoder.layer.{i}.attention.attention.key.weight"
-            )
-            key_bias = state_dict.pop(f"encoder.layer.{i}.attention.attention.key.bias")
-            value_weight = state_dict.pop(
-                f"encoder.layer.{i}.attention.attention.value.weight"
-            )
-            value_bias = state_dict.pop(
-                f"encoder.layer.{i}.attention.attention.value.bias"
-            )
-            output_weight = state_dict.pop(
-                f"encoder.layer.{i}.attention.output.dense.weight"
-            )
-            output_bias = state_dict.pop(
-                f"encoder.layer.{i}.attention.output.dense.bias"
-            )
-            self.encoder[i].attn.W_qkv.weight.data = torch.cat(
-                [query_weight, key_weight, value_weight], dim=0
-            )
-            self.encoder[i].attn.W_qkv.bias.data = torch.cat(
-                [query_bias, key_bias, value_bias], dim=0
-            )
-            self.encoder[i].attn.W_o.weight.data = output_weight
-            self.encoder[i].attn.W_o.bias.data = output_bias
-
-        # Load FFN
-        for i in range(self.n_layers):
-            fc1_weight = state_dict.pop(f"encoder.layer.{i}.intermediate.dense.weight")
-            fc1_bias = state_dict.pop(f"encoder.layer.{i}.intermediate.dense.bias")
-            fc2_weight = state_dict.pop(f"encoder.layer.{i}.output.dense.weight")
-            fc2_bias = state_dict.pop(f"encoder.layer.{i}.output.dense.bias")
-            self.encoder[i].ffn.fc1.weight.data = fc1_weight
-            self.encoder[i].ffn.fc1.bias.data = fc1_bias
-            self.encoder[i].ffn.fc2.weight.data = fc2_weight
-            self.encoder[i].ffn.fc2.bias.data = fc2_bias
-
-        # Load LayerNorm
-        for i in range(self.n_layers):
-            ln1_weight = state_dict.pop(f"encoder.layer.{i}.layernorm_before.weight")
-            ln1_bias = state_dict.pop(f"encoder.layer.{i}.layernorm_before.bias")
-            ln2_weight = state_dict.pop(f"encoder.layer.{i}.layernorm_after.weight")
-            ln2_bias = state_dict.pop(f"encoder.layer.{i}.layernorm_after.bias")
-
-            self.encoder[i].addnorm1.norm_layer.weight.data = ln1_weight
-            self.encoder[i].addnorm1.norm_layer.bias.data = ln1_bias
-            self.encoder[i].addnorm2.norm_layer.weight.data = ln2_weight
-            self.encoder[i].addnorm2.norm_layer.bias.data = ln2_bias
-
-        # Load Post LayerNorm
-        post_ln_weight = state_dict.pop("layernorm.weight")
-        post_ln_bias = state_dict.pop("layernorm.bias")
-        self.post_layernorm.weight.data = post_ln_weight
-        self.post_layernorm.bias.data = post_ln_bias
-
-        if len(state_dict) > 0:
-            raise ValueError("Some weights are not loaded")
-        else:
-            print("All weights are loaded!")
-
 
 class ViTRawModel(nn.Module):
     def __init__(self, config: ViTConfig) -> None:
@@ -201,10 +122,6 @@ class ViTRawModel(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.vit(x)
-
-    def from_hf(self, model: nn.Module) -> None:
-        state_dict = model.state_dict()
-        self.vit.from_hf(state_dict)
 
 
 class VitClassifier(nn.Module):
@@ -226,10 +143,3 @@ class VitClassifier(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.head(self.vit(x)[:, 0, :])
-
-    def from_hf(self, model: nn.Module) -> None:
-        state_dict = model.state_dict()
-        vit_state_dict = model.vit.state_dict()
-        self.vit.from_hf(vit_state_dict)
-        self.head.model.fc[0].weight.data = state_dict.pop("classifier.weight")
-        self.head.model.fc[0].bias.data = state_dict.pop("classifier.bias")
