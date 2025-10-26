22022968f5fd133930135a5cd708d603308ab833
dashygo097 <dashygo097@gmail.com>
2025-10-26 19:38:33 +0800

    fix: dependencies

--- src/ml/sys/quantization/ptq/base.py
+++ src/ml/sys/quantization/ptq/base.py
@@ -1,12 +1,8 @@
-from typing import Optional
-
-import torch
-import torchao as tq
 from torch import nn
 
 from ..quantizer import Quantizer
 
 
-class PTQQuantizer(Quantizer):
+class PTQ(Quantizer):
     def __init__(self, model: nn.Module) -> None:
         super().__init__(model)
