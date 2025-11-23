5d645b2c905f7b314ddcc8275d020f3c05356749
dashygo097 <dashygo097@github.com>
2025-11-23 18:08:18 +0800

    checkpoint

--- src/ml/typical/heads/depth_estimation/mlp_1x1.py
+++ src/ml/typical/heads/depth_estimation/mlp_1x1.py
@@ -13,6 +13,7 @@ class DepthEstimationMLPHead(nn.Module):
         hidden_features: Optional[List[int]] = None,
         use_bilinear: bool = True,
         act: type[nn.Module] = nn.ReLU,
+        out_act: nn.Module = nn.Identity(),
     ):
         super().__init__()
         
@@ -21,6 +22,7 @@ class DepthEstimationMLPHead(nn.Module):
         self.input_res = input_res
         self.use_bilinear = use_bilinear
         self.act = act
+        self.out_act = out_act
         
         h, w = input_res
         self.feat_h = h // patch_size
@@ -63,15 +65,13 @@ class DepthEstimationMLPHead(nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         batch_size, num_patches, embed_size = x.shape
         
-        # Remove CLS token if present
         if num_patches == self.feat_h * self.feat_w + 1:
             x = x[:, 1:, :] 
         
-        # Transpose and make contiguous for MPS compatibility
         x = x.transpose(1, 2).contiguous()  # [B, embed_size, num_patches]
         x = x.view(batch_size, embed_size, self.feat_h, self.feat_w)
         
         depth_low_res = self.mlp(x)
         depth_map = self.upsample(depth_low_res)
         
-        return depth_map.sigmoid()
+        return self.out_act(depth_map)
